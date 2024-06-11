0d229bdfcdfbaa4787160417698794fa97f8b7f5
shitoletushar3132 <shitoletushar3132@gmail.com>
2024-06-11 18:32:21 +0530

    day 46

--- day 48/vite-project/src/App.jsx
+++ day 48/vite-project/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import LudoBoard from "./LudoBoard";
-import TodoList from "./TodoList";
+import Lottery from "./Lottery";
+import { sum } from "./helper";
 
 function App() {
+  let winCondition = (ticket) => {
+    return sum(ticket) === 15;
+  };
   return (
     <>
-      <TodoList />
+      <Lottery n={3} winningSum={15} winCondition={winCondition} />
     </>
   );
 }
